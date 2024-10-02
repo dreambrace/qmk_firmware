9043934ce5e6ad8411bb0982a1dab3cff196a22d
dreambrace <98101206+dreambrace@users.noreply.github.com>
2024-10-02 22:38:22 +0200

    Add Dreambrace's keymap
    alt_oled_images are just previews of oled screen images I was
    experimenting with.
    If you want to use them for some reason, you can pull them through an
    image to byte-array converter like: https://javl.github.io/image2cpp/
    Just make sure you set the right settings for your oled.

---
+++ keyboards/crkbd/keymaps/dreambrace/rules.mk
@@ -0,0 +1,8 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+CONVERT_TO = promicro_rp2040
