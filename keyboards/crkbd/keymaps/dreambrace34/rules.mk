2c8540a2a963ef6d0c1f1cfe2a1ab207abe92376
dreambrace <98101206+dreambrace@users.noreply.github.com>
2024-10-04 04:51:56 +0200

    Add Dreambrace's "34" key layout
    Not actually a 34 key layout, but the extra thumb buttons are just
    stand-ins for a thumb combo which isn't very comforbatle on the corne.

---
+++ keyboards/crkbd/keymaps/dreambrace34/rules.mk
@@ -0,0 +1,10 @@
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+CONVERT_TO = promicro_rp2040
+DEFERRED_EXEC_ENABLE = yes
