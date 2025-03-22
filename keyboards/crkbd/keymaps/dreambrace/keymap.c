d55ed2742b2b81a5856d09383a53d17f7a02655d
dreambrace <98101206+dreambrace@users.noreply.github.com>
2025-03-22 21:35:27 +0100

    Add suffering

--- keyboards/crkbd/keymaps/dreambrace/keymap.c
+++ keyboards/crkbd/keymaps/dreambrace/keymap.c
@@ -279,11 +279,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_QWERTY] = LAYOUT_split_3x6_3(
-    XXXXXXX,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     XXXXXXX,
-    LS_WM,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                  KC_H,     KC_J,     KC_K,     KC_L,     KC_QUOT,  LS_MEDIA,
-    XXXXXXX,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  XXXXXXX,
+    XXXXXXX,  KC_Z,     KC_L,     KC_D,     KC_W,     KC_V,                  KC_J,     KC_F,     KC_O,     KC_U,     KC_SLSH,  XXXXXXX,
+    LS_WM,    KC_N,     KC_R,     KC_T,     KC_S,     KC_G,                  KC_Y,     KC_H,     KC_A,     KC_E,     KC_I,     LS_MEDIA,
+    XXXXXXX,  KC_Q,     KC_X,     KC_M,     KC_C,     KC_B,                  KC_K,     KC_P,     KC_QUOT,  KC_COMM,  KC_DOT,   XXXXXXX,
                                   LS_MOUSE, LS_NAV,   LS_SHIFT,              LS_SYM,   LS_NUM,   LS_FUNC
   ),
+
+  // [_QWERTY] = LAYOUT_split_3x6_3(
+  //   XXXXXXX,  KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,                  KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     XXXXXXX,
+  //   LS_WM,    KC_A,     KC_S,     KC_D,     KC_F,     KC_G,                  KC_H,     KC_J,     KC_K,     KC_L,     KC_QUOT,  LS_MEDIA,
+  //   XXXXXXX,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                  KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  XXXXXXX,
+  //                                 LS_MOUSE, LS_NAV,   LS_SHIFT,              LS_SYM,   LS_NUM,   LS_FUNC
+  // ),
 
   [_NAV] = LAYOUT_split_3x6_3(
     XXXXXXX,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     XXXXXXX,
