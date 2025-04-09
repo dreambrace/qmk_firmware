54530e231b5682170f2c940fcdb0ebfedce80d77
dreambrace <98101206+dreambrace@users.noreply.github.com>
2025-04-09 22:51:14 +0200

    Add orkuso layout
    Default Sofle layout adjusted for the rp2040 microcontroller

---
+++ keyboards/sofle/keymaps/orkuso/config.h
@@ -0,0 +1,13 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define MASTER_LEFT
+#define TAPPING_TERM 200
+#define QUICK_TAP_TERM 200
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 3
+#define TRI_LAYER_ADJUST_LAYER 4
---
+++ keyboards/sofle/keymaps/orkuso/rules.mk
@@ -0,0 +1,6 @@
+TRI_LAYER_ENABLE = yes
+CONVERT_TO = promicro_rp2040
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+LTO_ENABLE = yes
+
